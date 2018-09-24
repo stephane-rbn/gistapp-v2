7e9731d8f71703d8966339dba049a5ecd4ea39d8
Stéphane <rabena.stephane@gmail.com>
2018-09-25 00:24:54 +0200

    Add seeds to create gists

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Gist.destroy_all
+
+Gist.create(
+  filename: 'hello_world.rb',
+  description: "puts ",
+  content: "puts 'hello!'"
+)
+
+Gist.create(
+  filename: 'snake_game.py',
+  description: "If you like snakes, you'll like this piece of code",
+  content: 'print("SssSSss!")'
+)
+
+Gist.create(
+  filename: 'doYou.c',
+  description: "I like C but C doesn't like me",
+  content: 'printf("Beautiful gist!\n");'
+)
+
+Gist.create(
+  filename: 'magicAlgorithm.php',
+  description: "The algorithm that can resolve any problem",
+  content: '<?php echo "Magic!" ?>'
+)
+
+Gist.create(
+  filename: 'noInspiration.java',
+  description: "I have no inspiration for this one, sorry about that",
+  content: 'System.out.println("I do enjoy coding in Java, sometimes");'
+)
